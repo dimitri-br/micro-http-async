f4e5910c48e6de9beefeb4c9f7bd9d56c13f3680
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2021-02-12 15:40:59 +0000

    Added documentation

--- src/json_response.rs
+++ src/json_response.rs
@@ -1,8 +1,17 @@
 use crate::Response;
 
+/// # JSONResponse
+///
+/// Helpful tool for converting a serde_json json string into a response.
+///
+/// It will take in the response code, and create a response string with the headers required.
+///
+/// See the examples for usage
 pub struct JSONResponse;
 
 impl JSONResponse{
+    /// This function takes the response code and data (the json) to write, and returns a string that can be used
+    /// as a response.
     pub async fn construct_response(response_code: Response, data: String) -> String{
         // This should be changed over to support all response types
         let header_code = match response_code{
