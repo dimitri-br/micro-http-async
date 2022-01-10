8e3f02af389fd29967af81e4ecc8a52567ea20ba
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2022-01-10 23:51:59 +0000

    TLS WIP

--- src/connection.rs
+++ src/connection.rs
@@ -1,31 +1,54 @@
 use std::error::Error;
 use std::io;
 use tokio::net::TcpStream;
+use tokio_rustls::server::TlsStream;
+use tokio::io::{split, AsyncWriteExt, AsyncReadExt};
+
+
+
+pub enum ConnectionType{
+    Plain(Option<TcpStream>),
+    TLS(Option<TlsStream<TcpStream>>)
+}
 
 /// # Connection
 ///
 /// This struct is a helpful struct to handle the nitty gritty of
 /// connections, such as reading and writing to the stream
 pub struct Connection{
-    stream: TcpStream,
+    /// The connection type - plain or TLS
+    connection_type: ConnectionType,
+    /// The read buffer size
     read_buffer_size: usize,
+
+    /// We need to store the TLS stream so we don't run into copy/clone issues
+    tls_stream: Option<TlsStream<TcpStream>>,
 }
 
 impl Connection {
     /// # New
     ///
     /// Create a new connection handler from a `TcpStream`
-    pub fn new(stream: TcpStream, read_buffer_size: usize) -> Self {
+    pub fn new(mut connection_type: ConnectionType, read_buffer_size: usize) -> Self {
+        // Check if we need to use TLS
+        let tls_stream = match connection_type {
+            ConnectionType::TLS(ref mut tls_stream) => {
+                let stream = tls_stream.take().unwrap();
+                Some(stream)
+            },
+            _ => None,
+        };
         Connection{
-            stream,
+            connection_type,
             read_buffer_size,
+            tls_stream,
         }
     }
 
     /// # Read To String
     ///
     /// Read the `TcpStream` to a `String`
-    pub async fn read_to_string(&self) -> Result<String, &'static str> {
+    pub async fn read_to_string(&mut self) -> Result<String, &'static str> {
         let mut string = unsafe {
             String::from_utf8_unchecked(
                 self.read_to_vec()
@@ -42,62 +65,110 @@ impl Connection {
     /// # Read To Vec
     ///
     /// Read the `TcpStream` to a `Vec<u8>`. Returns a `Result` as we cannot guarantee a successful read.
-    pub async fn read_to_vec(&self) -> Result<Vec<u8>, Box<dyn Error>> {
+    pub async fn read_to_vec(&mut self) -> Result<Vec<u8>, Box<dyn Error>> {
         let mut buffer = Vec::with_capacity(self.read_buffer_size);
 
-        let stream: &TcpStream = &self.stream; // Get a reference to the stream
+        
+        match self.connection_type {
+            ConnectionType::Plain(ref mut stream) => {
+                let stream = stream.as_mut().unwrap();
+                // We loop while we're waiting for a read
+                loop {
+                    stream.readable().await?; // we await for the stream to be readable
 
-        // We loop while we're waiting for a read
-        loop {
-            stream.readable().await?; // we await for the stream to be readable
+                    // Try to read data, this may still fail with `WouldBlock`
+                    // if the readiness event is a false positive.
+                    match stream.try_read_buf(&mut buffer) {
+                        Ok(0) => break, // No data recieved
+                        Ok(_) => {
+                            break; // we recieved some data, just break the loop and return it
+                        }
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            continue; // The IO is busy and would block - just try again
+                        }
+                        Err(e) => {
+                            return Err(e.into()); // Some other error - quit
+                        }
+                    }
+                }
+                let buffer: Vec<u8> = buffer
+                    .iter()
+                    .filter(|x| **x != 0x0u8)
+                    .map(|x| *x)
+                    .collect(); // Remove all empty bytes (to avoid trailing whitespaces)
 
-            // Try to read data, this may still fail with `WouldBlock`
-            // if the readiness event is a false positive.
-            match stream.try_read_buf(&mut buffer) {
-                Ok(0) => break, // No data recieved
-                Ok(_) => {
-                    break; // we recieved some data, just break the loop and return it
-                }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    continue; // The IO is busy and would block - just try again
-                }
-                Err(e) => {
-                    return Err(e.into()); // Some other error - quit
-                }
+                return Ok(buffer) // return our vector;
+
+            },
+            ConnectionType::TLS(_) => {
+                // Unfortunately, the TLS stream doesn't implement `AsyncRead`
+                // so we have to use the `split` combinator to split the stream
+                // This is a bit of a hack, but it works. We also lose 
+                // async/await support, but that's ok for now.
+                let tls_stream = self.tls_stream.take().unwrap();
+                let (mut reader, writer) = split(tls_stream);
+
+                // Try to read data, this may still fail with `WouldBlock`
+                // if the readiness event is a false positive.
+                reader.read_buf(&mut buffer).await.unwrap();
+
+                let buffer: Vec<u8> = buffer
+                    .iter()
+                    .filter(|x| **x != 0x0u8)
+                    .map(|x| *x)
+                    .collect(); // Remove all empty bytes (to avoid trailing whitespaces)
+
+                // Reconnect the stream
+                self.tls_stream = Some(reader.unsplit(writer));
+
+                return Ok(buffer) // return our vector;
             }
-        }
-        let buffer: Vec<u8> = buffer
-            .iter()
-            .filter(|x| **x != 0x0u8)
-            .map(|x| *x)
-            .collect(); // Remove all empty bytes (to avoid trailing whitespaces)
-        Ok(buffer) // return our vec
+        };
     }
 
     /// # Write String
     ///
     /// Write a `String` value to the `TcpStream`. Returns a `Result` as we cannot guarantee a successful write.
     pub async fn write_string(&mut self, data: String) -> Result<(), Box<dyn Error>> {
-        let stream: &mut TcpStream = &mut self.stream; // Get a reference to the stream
+        match self.connection_type{
+            ConnectionType::Plain(ref mut stream) => {
+                let stream = stream.as_mut().unwrap();
 
-        loop {
-            // Wait for the socket to be writable
-            stream.writable().await?;
+                loop {
+                    // Wait for the socket to be writable
+                    stream.writable().await?;
+        
+                    // See `read_to_vec` for more explaination what happens here
+                    //
+                    // Try to write data, this may still fail with `WouldBlock`
+                    // if the readiness event is a false positive.
+                    match stream.try_write(data.as_bytes()) {
+                        Ok(_) => {
+                            break;
+                        }
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            continue;
+                        }
+                        Err(e) => {
+                            return Err(e.into());
+                        }
+                    }
+                }
+            },
+            ConnectionType::TLS(_) =>{
+                // Unfortunately, the TLS stream doesn't implement `AsyncWrite`
+                // so we have to use the `sink` combinator to split the stream
+                // This is a bit of a hack, but it works. We also lose 
+                // async/await support, but that's ok for now.
+                let tls_stream = self.tls_stream.take().unwrap();
+                let (reader, mut writer) = split(tls_stream);
 
-            // See `read_to_vec` for more explaination what happens here
-            //
-            // Try to write data, this may still fail with `WouldBlock`
-            // if the readiness event is a false positive.
-            match stream.try_write(data.as_bytes()) {
-                Ok(_) => {
-                    break;
-                }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                Err(e) => {
-                    return Err(e.into());
-                }
+                // Try to write data, this may still fail with `WouldBlock`
+                // if the readiness event is a false positive.
+                writer.write_all(data.as_bytes()).await?;
+
+                // Reconnect the stream
+                self.tls_stream = Some(reader.unsplit(writer));
             }
         }
 
@@ -108,28 +179,47 @@ impl Connection {
     ///
     /// Write bytes to the TCP stream, useful for sending data for things such as images or downloadable binary files
     pub async fn write_bytes(&mut self, data: Vec<u8>) -> Result<(), Box<dyn Error>> {
-        let stream: &mut TcpStream = &mut self.stream; // Get a reference to the stream
+        match self.connection_type{
+            ConnectionType::Plain(ref mut stream) => {
+                let stream = stream.as_mut().unwrap();
 
-        loop {
-            // Wait for the socket to be writable
-            stream.writable().await?;
+                loop {
+                    // Wait for the socket to be writable
+                    stream.writable().await?;
+        
+                    // See `read_to_vec` for more explaination what happens here
+                    //
+                    // Try to write data, this may still fail with `WouldBlock`
+                    // if the readiness event is a false positive.
+                    match stream.try_write(&data) {
+                        Ok(_) => {
+                            break;
+                        }
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            continue;
+                        }
+                        Err(e) => {
+                            return Err(e.into());
+                        }
+                    }
+                }
+            },
+            ConnectionType::TLS(_) => {
+                // Unfortunately, the TLS stream doesn't implement `AsyncWrite`
+                // so we have to use the `sink` combinator to split the stream
+                // This is a bit of a hack, but it works. We also lose 
+                // async/await support, but that's ok for now.
+                let tls_stream = self.tls_stream.take().unwrap();
+                let (reader, mut writer) = split(tls_stream);
 
-            // See `read_to_vec` for more explaination what happens here
-            //
-            // Try to write data, this may still fail with `WouldBlock`
-            // if the readiness event is a false positive.
-            match stream.try_write(&data) {
-                Ok(_) => {
-                    break;
-                }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                Err(e) => {
-                    return Err(e.into());
-                }
+                // Try to write data, this may still fail with `WouldBlock`
+                // if the readiness event is a false positive.
+                writer.write_all(&data).await?;
+
+                // Reconnect the stream
+                self.tls_stream = Some(reader.unsplit(writer));
             }
-        }
+        };
 
         Ok(())
     }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,17 @@
-use tokio::io;
+use std::io::BufReader;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+
+use tokio::io::{self};
 use tokio::net::{TcpListener, TcpStream}; // Async versions of the stdlib implementation // :D
 
-use crate::Connection;
+// TLS stuff, so we can support HTTPS
+use tokio_rustls::rustls::{self, Certificate, PrivateKey};
+use tokio_rustls::{TlsAcceptor, server::TlsStream};
+use rustls_pemfile::{certs, rsa_private_keys};
+
+use crate::{Connection, connection::ConnectionType};
 use crate::Routes;
 
 /// # HTTP Server
@@ -19,6 +29,9 @@ pub struct HttpServer {
 
     /* Hidden parameters */
     read_buffer_size: usize,
+
+    // TLS stuff
+    tls_acceptor: Option<TlsAcceptor>,
 }
 
 impl HttpServer {
@@ -37,6 +50,29 @@ impl HttpServer {
             listener: TcpListener::bind(&address).await?,
             routes: Routes::new().await,
             read_buffer_size: 8192,
+            tls_acceptor: None,
+        })
+    }
+
+    pub async fn new_tls(ip: &str, port: &str, cert_path: PathBuf, key_path: PathBuf) -> io::Result<Self> {
+        let address = format!("{}:{}", ip, port);
+        println!("Listening on {}", address);
+        let certs = load_certs(&cert_path)?;
+        let mut keys = load_keys(&key_path)?;
+
+        let config = rustls::ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(certs, keys.remove(0))
+            .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
+
+        let acceptor = TlsAcceptor::from(Arc::new(config));
+
+        Ok(Self {
+            listener: TcpListener::bind(&address).await?,
+            routes: Routes::new().await,
+            read_buffer_size: 8192,
+            tls_acceptor: Some(acceptor),
         })
     }
 
@@ -48,7 +84,16 @@ impl HttpServer {
     pub async fn listen(&mut self) -> Result<(), &'static str> {
         loop {
             let (socket, addr) = self.listener.accept().await.unwrap(); // Accept an incoming connection
-            self.handle_connection(socket, addr).await.unwrap(); // Handle it
+
+            let acceptor = self.tls_acceptor.clone();
+
+            // Check if we need to use TLS
+            if let Some(ref tls_acceptor) = acceptor {
+                let tls_socket = tls_acceptor.accept(socket).await.unwrap();
+                self.handle_connection_tls(tls_socket, addr).await.unwrap();
+            }else{
+                self.handle_connection(socket, addr).await.unwrap(); // Handle it
+            }
         }
     }
 
@@ -68,7 +113,31 @@ impl HttpServer {
         stream: TcpStream,
         addr: std::net::SocketAddr,
     ) -> Result<(), &str> {
-        let mut connection = Connection::new(stream, self.read_buffer_size); // Create our connection handler
+        let mut connection = Connection::new(ConnectionType::Plain(Some(stream)), self.read_buffer_size); // Create our connection handler
+
+        let request_str = connection.read_to_string().await.unwrap(); // get a string value from the recieved data
+
+        // only needs the request and address as it constructs a `Request` to get the route and more info
+        let ret_str = self.routes.get_route(request_str, addr).await.unwrap();
+
+        match ret_str {
+            crate::DataType::Text(text) => {
+                connection.write_string(text).await.unwrap();
+            }
+            crate::DataType::Bytes(bytes) => {
+                connection.write_bytes(bytes).await.unwrap();
+            }
+        }
+
+        Ok(()) // Return the future
+    }
+
+    async fn handle_connection_tls(
+        &mut self,
+        stream: TlsStream<TcpStream>,
+        addr: std::net::SocketAddr,
+    ) -> Result<(), &str> {
+        let mut connection = Connection::new(ConnectionType::TLS(Some(stream)), self.read_buffer_size); // Create our connection handler
 
         let request_str = connection.read_to_string().await.unwrap(); // get a string value from the recieved data
 
@@ -95,4 +164,32 @@ impl HttpServer {
         
         Ok(())
     }
+}
+
+
+// Helpful TLS functions for reading certificates and keys
+
+/// # Load Certificates
+/// 
+/// Load certificates from a given path.
+/// 
+/// Returns a `Vec<Certificate>`
+fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
+    let buf_reader = &mut BufReader::new(File::open(path).unwrap());
+    certs(buf_reader)
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
+        .map(|mut certs| certs.drain(..).map(Certificate).collect())
+}
+
+
+/// # Load Keys
+/// 
+/// Load keys from a given path.
+/// 
+/// Returns a `Vec<PrivateKey>`
+fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
+    let buf_reader = &mut BufReader::new(File::open(path).unwrap());
+    rsa_private_keys(buf_reader)
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
+        .map(|mut keys| keys.drain(..).map(PrivateKey).collect())
 }
